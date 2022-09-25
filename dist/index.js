1626c4bc15c2b219f94c914984d91890c22c40a8
Lara Destein <laradestein@Laras-MacBook-Pro.local>
2022-09-25 16:48:56 -0700

    get program to read excel columns and rows

--- dist/index.js
+++ dist/index.js
@@ -1,9 +1,36 @@
-"use strict";
+'use strict';
 
-var _process = require("process");
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
+var _node = require('read-excel-file/node');
+
+var _node2 = _interopRequireDefault(_node);
+
+var _process = require('process');
 
 var _process2 = _interopRequireDefault(_process);
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-console.log("hello");
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+var Tracker = function () {
+    function Tracker() {
+        _classCallCheck(this, Tracker);
+    }
+
+    _createClass(Tracker, [{
+        key: 'run',
+        value: async function run() {
+            console.log("run");
+            var filepath = '/Users/laradestein/git/incident-tracker/test-data/sfs.xlsx';
+            var ret = await (0, _node2.default)(filepath);
+            console.log(JSON.stringify(ret, null, 4));
+        }
+    }]);
+
+    return Tracker;
+}();
+
+var tracker = new Tracker();
+tracker.run();
--- index.js
+++ index.js
@@ -1,3 +1,16 @@
+import readXlsxFile from 'read-excel-file/node';
 import process from 'process';
 
-console.log("hello");
+class Tracker {
+    async run() {
+        console.log("run");
+        const filepath = '/Users/laradestein/git/incident-tracker/test-data/sfs.xlsx';
+        const ret = await readXlsxFile(filepath);
+        console.log(JSON.stringify(ret, null, 4));
+    }
+} 
+
+
+
+const tracker = new Tracker();
+tracker.run();
