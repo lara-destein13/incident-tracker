e0715fcb144bb3278375e9994c222c09157a7298
Lara Destein <laradestein@Laras-MacBook-Pro.local>
2022-09-25 16:02:28 -0700

    get the app running

---
+++ dist/index.js
@@ -0,0 +1,9 @@
+"use strict";
+
+var _process = require("process");
+
+var _process2 = _interopRequireDefault(_process);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+console.log("hello");
